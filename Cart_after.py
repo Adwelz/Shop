8da01539b5292ead63b0cb8b92603f203e9f88c1
Adwelz <43895468+Adwelz@users.noreply.github.com>
2023-03-01 17:27:05 +0100

    f

--- Cart_after.py
+++ Cart_after.py
@@ -1,17 +1,12 @@
 from Item import Item
 from Shop import Shop
+
 class Cart:
-
-    global freeship_price # global data
-    freeship_price = 100
-
     def __init__(self, id, client) -> None:
         self.id = id
         self.client = client
         self.client.carts.append(id)
         self.items = []
-        if self.client.membership==True:
-            freeship_price=10
 
     def add2Cart(self,id_item):
         self.items.append(id_item)
@@ -27,5 +22,9 @@ class Cart:
         return sum
     
     def freeship(self):
+        if self.client.membership==True:
+            freeship_price=10
+        else:
+            freeship_price=100
         if self.getCartPrice()>freeship_price:
             return True
--- Item.py
+++ Item.py
@@ -1,6 +1,6 @@
 class Item:
     def __init__(self,id, nbr_left, price) -> None:
         self.id = id
-        self.nbr = nbr_left
+        self.nbr_left = nbr_left
         self.price = price
 
