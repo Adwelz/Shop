2b706db24ea437276dd2b9ca9997972e1fa3527f
Adwelz <43895468+Adwelz@users.noreply.github.com>
2023-03-01 13:26:30 +0100

    3

--- Order.py
+++ Order.py
@@ -1,4 +1,5 @@
 from Cart import Cart
+from Membership import Membership
 
 class Order:
     def __init__(self,id,client) -> None:
@@ -9,4 +10,7 @@ class Order:
         for cart in self.client.carts:
             sum = 0
             sum+=cart.getCartPrice()
+
+        if client.membership == True:
+            sum = Membership.priceWithMembership(sum)
         return sum
